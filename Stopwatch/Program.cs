b2321ca97c537282daaf92919594a89c852a4c3a
marcos <marcoseu599@gmail.com>
2023-10-08 14:35:10 -0300

    projeto

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Start();         
+            Console.ReadKey();
+        }
+
+        static int Menu()
+            {
+                Console.WriteLine("Cronometro");
+                Console.WriteLine("Quantos segundos deseja contar: \n"+ 
+                                  "Caso deseje sair, basta digitar sair: ");
+                int tempo = int.Parse(Console.ReadLine());
+                return tempo;
+            }
+
+        static void Start()
+        { 
+
+            int time = Menu();
+            int inicio = 0;
+
+            while (inicio <= time)
+            {
+                Console.Clear();
+                Console.WriteLine(inicio);
+                Thread.Sleep(1000);
+                inicio +=1;
+            }
+
+            Final();
+            
+        }    
+
+        static void Final()
+        {
+            Console.WriteLine("Agora, decida:");
+            Console.WriteLine("1 - Encerrar ");
+            Console.WriteLine("2 - Continuar ");
+            int finalizar = int.Parse(Console.ReadLine());
+
+            if (finalizar == 2)
+            {
+                Start();
+            } else if (finalizar != 1 && finalizar != 2)
+            {
+                Console.Clear();
+                Final();
+            }
+            else
+            {
+                Console.WriteLine("Até mais");
+            }
+        }
+                
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Teste {
+    
+    class Program {
+        static void Main (string[]args){
+
+            // byte test = 3;
+            // sbyte menegativo = -5;
+            // int numero = 10;
+            // numero = 20;
+            // short tes = 1;
+            // ushort test = -1; // erro
+            // double salario = 1.2333;
+            // float salarionovo = 2.12f;
+            // decimal salrio = 2.123m;
+            //char letra = 's';
+            //string palavra ="testando";
+            // int idade = null; // errado
+            // int? idade2 = null; // certo
+            // int inteiro = 100;
+            // float  real = 25.5f;
+            // double testando = inteiro - real;
+            // real = inteiro;
+            // inteiro = real; // erro
+            
+            // int inteiro = 100;
+            // Console.WriteLine(inteiro);
+
+            // if ( 25 == 32) // COMPAREI SE É IGUAL
+            // {
+            //     Console.WriteLine ( "TESTE 1 Correto");
+            // } else {
+            //     Console.WriteLine ("TESTE 1 Não igual");
+            // }
+
+            // if ( 25 != 32)  // COMPAREI SE NÃO É IGUAL
+            // {
+            //     Console.WriteLine ( "TESTE 2 Correto");
+            // } else {
+            //     Console.WriteLine ("TESTE 2 Não igual");
+            // }
+            
+            // if ( idade == maior_idade) // COMPAREI SE É IGUAL
+            // {
+            //     Console.WriteLine ( "TESTE 1 Correto");
+            // } else {
+            //     Console.WriteLine ("TESTE 1 Não igual");
+            // }
+
+            // if ( idade != maior_idade)  // COMPAREI SE NÃO É IGUAL
+            // {
+            //     Console.WriteLine ( "TESTE 2 Correto");
+            // } else {
+            //     Console.WriteLine ("TESTE 2 Não igual");
+            // }
+
+            // if ( idade >= maior_idade) // COMPAREI SE É maior
+            // {
+            //     Console.WriteLine ( "Maior");
+            // } else {
+            //     Console.WriteLine ("menor");
+            // }
+
+            // if ( idade >= maior_idade && idade <= idoso) // COMPARação com and 
+            // {
+            //     Console.WriteLine ( "Adulto");
+            // } else {
+            //     Console.WriteLine ("não classificado");
+            // }
+            // int idoso = 65;
+            // int idade = 08;
+            // int maior_idade = 18;
+            
+            // if ( idade >= maior_idade ) // COMPARação com else if
+            // {
+            //     Console.WriteLine ( "Adulto");
+            // } else if (idade <= idoso){
+            //     Console.WriteLine ("Usei o else if");
+            // }else {
+            //     Console.WriteLine ("não classificado");
+            // }
+
+            // string number = "33";
+            // //double res = number % 2;
+            // switch (int.Parse(number) % 2) {
+            //     case 0: Console.WriteLine(" É PAR");break;
+            //     case > 0:  Console.WriteLine(" É impar");break;
+            //     default : Console.WriteLine ("Não calculado"); break;
+            // }
+
+            // LAÇOS de repetição
+
+            for (var i = 0; i < 4; i++){
+                Console.WriteLine (i);
+            }
+
+            int varia = 5;
+            
+            while ( varia <= 7) {
+                 Console.WriteLine("while " + varia);
+                varia++;
+            }
+            
+            do {
+                    Console.WriteLine ("do while " + varia);
+                    //varia = varia - 1;
+                    //varia -=1;
+                    varia --;
+            }
+            while( varia > 0);    
+            
+        }
+    }
+}
